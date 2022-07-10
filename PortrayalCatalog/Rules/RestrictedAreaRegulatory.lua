6794d5fd5100a222c5088fad45d8cd62c78153ec
David Grant <62906211+DavidGrant-NIWC@users.noreply.github.com>
2022-07-09 20:19:04 -0400

    Fixes #95

--- PortrayalCatalog/Rules/RestrictedAreaRegulatory.lua
+++ PortrayalCatalog/Rules/RestrictedAreaRegulatory.lua
@@ -1,4 +1,7 @@
 -- RestrictedAreaRegulatory main entry point.
+-- #95
+
+require 'RestrictedAreaNavigational'
 
 function RestrictedAreaRegulatory(feature, featurePortrayal, contextParameters)
 	return RestrictedAreaNavigational(feature, featurePortrayal, contextParameters)
