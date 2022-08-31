f8c8af35f068734872549eeeedf5341eacab6d15
Mikan S <mikan.stamenkovich@navy.mil>
2022-08-31 13:17:44 -0400

    Implement portrayal rule for Pilotage District with new symbology. PSWG #57.

--- PortrayalCatalog/Rules/PilotageDistrict.lua
+++ PortrayalCatalog/Rules/PilotageDistrict.lua
@@ -1,14 +1,22 @@
 -- PilotageDistrict portrayal rules file.
+--
+-- ISSUES: PSWG #57, PC #94
 
 -- Main entry point for feature type.
 function PilotageDistrict(feature, featurePortrayal, contextParameters)
-	if feature.PrimitiveType == PrimitiveType.Surface then
-		featurePortrayal:AddInstructions('ViewingGroup:28010;DrawingPriority:12;DisplayPlane:UnderRADAR')
+	local viewingGroup = 28010
 
-		featurePortrayal:AddInstructions('PointInstruction:testPCB')
-		featurePortrayal:SimpleLineStyle('dash',0.64,'TRFCF')
-		featurePortrayal:AddInstructions('LineInstruction:_simple_')
+	if feature.PrimitiveType == PrimitiveType.Surface then
+		if contextParameters.RadarOverlay then
+			featurePortrayal:AddInstructions('ViewingGroup:28010;DrawingPriority:18;DisplayPlane:OverRADAR')
+		else
+			featurePortrayal:AddInstructions('ViewingGroup:28010;DrawingPriority:18;DisplayPlane:UnderRADAR')
+		end
+				
+		featurePortrayal:AddInstructions('LineInstruction:PILDSTR1')
 	else
 		error('Invalid primitive type or mariner settings passed to portrayal')
 	end
+
+	return viewingGroup
 end
