9c65be3972df679baaa73cbd0c1522fe65ff81ad
David Grant <62906211+DavidGrant-NIWC@users.noreply.github.com>
2022-09-08 16:08:11 -0400

    Fixes #98

--- PortrayalCatalog/Rules/Pile.lua
+++ PortrayalCatalog/Rules/Pile.lua
@@ -1,5 +1,6 @@
 -- Converter Version: 0.99
 -- Feature Catalogue Version: 1.0.0 (2019/4/9)
+-- #98: curve and surface geometry support
 
 -- Pile main entry point.
 function Pile(feature, featurePortrayal, contextParameters)
@@ -17,15 +18,21 @@ function Pile(feature, featurePortrayal, contextParameters)
 		end
 		featurePortrayal:AddInstructions('PointInstruction:PILPNT02')
 	elseif feature.PrimitiveType == PrimitiveType.Curve then
-		--TODO: Determine S-101 portrayal for Pile curve feature.
-		viewingGroup = 21010
-		featurePortrayal:AddInstructions('ViewingGroup:21010;DrawingPriority:15;PointInstruction:testPCB')
-		Debug.Trace('Warning: S-52 does not define portrayal for Pile curve features.')
+		viewingGroup = 12410
+		if contextParameters.RadarOverlay then
+			featurePortrayal:AddInstructions('ViewingGroup:12410;DrawingPriority:15;DisplayPlane:OverRADAR')
+		else
+			featurePortrayal:AddInstructions('ViewingGroup:12410;DrawingPriority:15;DisplayPlane:UnderRADAR')
+		end
+		featurePortrayal:AddInstructions('LineInstruction:PILPNT01')
 	elseif feature.PrimitiveType == PrimitiveType.Surface then
-		--TODO: Determine S-101 portrayal for Pile surface feature.
-		viewingGroup = 21010
-		featurePortrayal:AddInstructions('ViewingGroup:21010;DrawingPriority:15;PointInstruction:testPCB')
-		Debug.Trace('Warning: S-52 does not define portrayal for Pile surface features.')
+		viewingGroup = 12410
+		if contextParameters.RadarOverlay then
+			featurePortrayal:AddInstructions('ViewingGroup:12410;DrawingPriority:15;DisplayPlane:OverRADAR')
+		else
+			featurePortrayal:AddInstructions('ViewingGroup:12410;DrawingPriority:15;DisplayPlane:UnderRADAR')
+		end
+		featurePortrayal:AddInstructions('LineInstruction:PILPNT01')
 	else
 		error('Invalid primitive type or mariner settings passed to portrayal')
 	end
