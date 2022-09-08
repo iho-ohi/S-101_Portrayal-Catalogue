f241a4624c5905fddc6299479a70708b852573e4
David Grant <62906211+DavidGrant-NIWC@users.noreply.github.com>
2022-09-08 17:25:07 -0400

    Fixes #110

--- PortrayalCatalog/Rules/InformationArea.lua
+++ PortrayalCatalog/Rules/InformationArea.lua
@@ -1,25 +1,17 @@
 -- Information area main entry point.
+-- #110
+
 function InformationArea(feature, featurePortrayal, contextParameters)
+	featurePortrayal:AddInstructions('ViewingGroup:26200;DrawingPriority:12;DisplayPlane:UnderRADAR')
 	if feature.PrimitiveType == PrimitiveType.Point then
 		-- Simplified and paper chart points use the same symbolization
-		if contextParameters.RadarOverlay then
-			featurePortrayal:AddInstructions('ViewingGroup:26200;DrawingPriority:12;DisplayPlane:OverRADAR')
-		else
-			featurePortrayal:AddInstructions('ViewingGroup:26200;DrawingPriority:12;DisplayPlane:UnderRADAR')
-		end
-		featurePortrayal:AddInstructions('PointInstruction:testPCB')
+		featurePortrayal:AddInstructions('PointInstruction:INFARE01')
 	elseif feature.PrimitiveType == PrimitiveType.Curve then
-		featurePortrayal:AddInstructions('ViewingGroup:26200;DrawingPriority:12;DisplayPlane:UnderRADAR')
-		featurePortrayal:SimpleLineStyle('dash',0.64,'CHGRD')
-		featurePortrayal:AddInstructions('LineInstruction:_simple_')
-		--featurePortrayal:AddInstructions('LineInstruction:testPCB')
+		featurePortrayal:AddInstructions('LineInstruction:INFARE51')
 	elseif feature.PrimitiveType == PrimitiveType.Surface then
 		-- Plain and symbolized boundaries use the same symbolization
-		featurePortrayal:AddInstructions('ViewingGroup:26200;DrawingPriority:12;DisplayPlane:UnderRADAR')
-		featurePortrayal:SimpleLineStyle('dash',0.64,'CHGRD')
-		featurePortrayal:AddInstructions('LineInstruction:_simple_')
-		--featurePortrayal:AddInstructions('PointInstruction:testPCB')
-		--featurePortrayal:AddInstructions('LineInstruction:testPCB')
+		featurePortrayal:AddInstructions('PointInstruction:INFARE01')
+		featurePortrayal:AddInstructions('LineInstruction:INFARE51')
 	else
 		error('Invalid primitive type or mariner settings passed to portrayal')
 	end
