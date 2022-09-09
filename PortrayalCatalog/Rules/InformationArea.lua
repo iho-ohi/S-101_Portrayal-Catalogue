6a38fc0152b04623d2371cbcf40f46b3da40b852
David Grant <62906211+DavidGrant-NIWC@users.noreply.github.com>
2022-09-09 19:46:10 -0400

    Fixes #110 with updated viewing group and portrayal for InformationArea

--- PortrayalCatalog/Rules/InformationArea.lua
+++ PortrayalCatalog/Rules/InformationArea.lua
@@ -2,19 +2,26 @@
 -- #110
 
 function InformationArea(feature, featurePortrayal, contextParameters)
-	featurePortrayal:AddInstructions('ViewingGroup:26200;DrawingPriority:12;DisplayPlane:UnderRADAR')
 	if feature.PrimitiveType == PrimitiveType.Point then
 		-- Simplified and paper chart points use the same symbolization
+		if contextParameters.RadarOverlay then
+			featurePortrayal:AddInstructions('ViewingGroup:31020;DrawingPriority:12;DisplayPlane:OverRADAR')
+		else
+			featurePortrayal:AddInstructions('ViewingGroup:31020;DrawingPriority:12;DisplayPlane:UnderRADAR')
+		end
 		featurePortrayal:AddInstructions('PointInstruction:INFARE01')
 	elseif feature.PrimitiveType == PrimitiveType.Curve then
+		featurePortrayal:AddInstructions('ViewingGroup:31020;DrawingPriority:12;DisplayPlane:UnderRADAR')
 		featurePortrayal:AddInstructions('LineInstruction:INFARE51')
 	elseif feature.PrimitiveType == PrimitiveType.Surface then
 		-- Plain and symbolized boundaries use the same symbolization
-		featurePortrayal:AddInstructions('PointInstruction:INFARE01')
-		featurePortrayal:AddInstructions('LineInstruction:INFARE51')
+		featurePortrayal:AddInstructions('ViewingGroup:31020;DrawingPriority:12;DisplayPlane:UnderRADAR')
+		featurePortrayal:AddInstructions('PointInstruction:INFARE51')
+		featurePortrayal:AddInstructions('Dash:2,6;LineStyle:_simple_,8,0.32,CHMGF')
+		featurePortrayal:AddInstructions('LineInstruction:_simple_')
 	else
 		error('Invalid primitive type or mariner settings passed to portrayal')
 	end
 
-	return 26200
+	return 31020
 end
