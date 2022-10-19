4d50d4fff8f0b6fa8c2145cc3fcb939106d8b9f1
David Grant <62906211+DavidGrant-NIWC@users.noreply.github.com>
2022-10-19 17:45:30 -0400

    Fixes #65

--- PortrayalCatalog/Rules/DistanceMark.lua
+++ PortrayalCatalog/Rules/DistanceMark.lua
@@ -7,37 +7,39 @@ function DistanceMark(feature, featurePortrayal, contextParameters)
 	local viewingGroup
 
 	if feature.PrimitiveType == PrimitiveType.Point then
+		viewingGroup = 27030
+		if contextParameters.RadarOverlay then
+			featurePortrayal:AddInstructions('ViewingGroup:27030;DrawingPriority:21;DisplayPlane:OverRADAR')
+		else
+			featurePortrayal:AddInstructions('ViewingGroup:27030;DrawingPriority:21;DisplayPlane:UnderRADAR')
+		end
+		local textColor = 'CHMGD'
 		local structures = feature:GetFeatureAssociations('StructureEquipment')
-		if next(structures) == nil then
+		local noStructure = next(structures) == nil
+		if noStructure then
 			-- no StructureEquipment relationship, add a symbol
-			viewingGroup = 27030
-			if contextParameters.RadarOverlay then
-				featurePortrayal:AddInstructions('ViewingGroup:27030;DrawingPriority:21;DisplayPlane:OverRADAR')
-			else
-				featurePortrayal:AddInstructions('ViewingGroup:27030;DrawingPriority:21;DisplayPlane:UnderRADAR')
-			end
 			featurePortrayal:AddInstructions('PointInstruction:DISMAR06')
 		else
-			-- TODO: assign viewing group from structure?
-			viewingGroup = 27030
+			-- TODO: assign viewing group from structure so that text turns off with symbol?
+			textColor = 'CHBLK'
 		end
-		
+
 		if feature.measuredDistanceValue
 		then
 			local label = ''
 			
-			local distanceUnitMeasurement = feature.measuredDistanceValue.distanceUnitMeasurement
-			if distanceUnitMeasurement
+			local distanceUnitOfMeasurement = feature.measuredDistanceValue.distanceUnitOfMeasurement
+			if distanceUnitOfMeasurement
 			then
-				if distanceUnitMeasurement == 1	then
-					label = 'm'
-				elseif distanceUnitMeasurement == 2 then
+				if distanceUnitOfMeasurement == 1	then
+					label = 'M'
+				elseif distanceUnitOfMeasurement == 2 then
 					label = 'yd'
-				elseif distanceUnitMeasurement == 3 then
+				elseif distanceUnitOfMeasurement == 3 then
 					label = 'km'
-				elseif distanceUnitMeasurement == 4 then
-					label = 'sm'
-				elseif distanceUnitMeasurement == 5 then
+				elseif distanceUnitOfMeasurement == 4 then
+					label = 'StM'
+				elseif distanceUnitOfMeasurement == 5 then
 					label = 'nm'
 				end
 			end
@@ -47,8 +49,14 @@ function DistanceMark(feature, featurePortrayal, contextParameters)
 			then
 				label = label .. waterwayDistance
 			end
+			
+			if noStructure then
+				featurePortrayal:AddInstructions('LocalOffset:3.15,0')
+			else
+				featurePortrayal:AddInstructions('LocalOffset:-7.02,0;TextAlignHorizontal:End')
+			end
 		
-			featurePortrayal:AddInstructions('LocalOffset:2.106,0;FontWeight:Light;FontSize:6;FontColor:CHMGD')
+			featurePortrayal:AddInstructions('TextAlignVertical:Center;FontWeight:Light;FontColor:' .. textColor)
 			featurePortrayal:AddTextInstruction(EncodeString(label), 21, 24, viewingGroup, 21)
 		end
 	else
