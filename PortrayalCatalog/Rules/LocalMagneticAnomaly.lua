6be2dd04da1f63820e4087f557cc4e2106d7ecff
Mikan Stamenkovich <mikan.stamenkovich@navy.mil>
2023-02-08 13:23:40 -0500

    Work in progress issue #73 PC, #102 PSWG.  PilotBoardingPlace as proxy.  Also portrayal_catalogue.xml saved as UTF-8 instead of ANSI.

--- PortrayalCatalog/Rules/LocalMagneticAnomaly.lua
+++ PortrayalCatalog/Rules/LocalMagneticAnomaly.lua
@@ -37,16 +37,72 @@ function LocalMagneticAnomaly(feature, featurePortrayal, contextParameters)
 		error('Invalid primitive type or mariner settings passed to portrayal')
 	end
 
-		-- issue #73
-		
-		if feature.valueOfLocalMagneticAnomaly then	-- complex attribute
-			--Debug.Break()
-			if feature.valueOfLocalMagneticAnomaly.magneticAnomalyValueMaximum then
-				featurePortrayal:AddInstructions('LocalOffset:3.51,3.51;FontSize:10')
-				featurePortrayal:AddTextInstruction(EncodeString(feature.valueOfLocalMagneticAnomaly.magneticAnomalyValueMaximum, '%3.1f\''), 30, 24, viewingGroup, 12)
-			end
-		end
-		-- end #73
+		-- issue #73 PC, #102 PSWG
 
+	-- replace with local valueOfLocalMagneticAnomaly = feature.valueOfLocalMagneticAnomaly 
+	-- valueOf multiplicity is 1:2
+	--local valueOfLocalMagneticAnomaly = {'1', nil} -- replace with local valueOfLocalMagneticAnomaly = feature.valueOfLocalMagneticAnomaly  
+	local valueOfLocalMagneticAnomaly = {}
+	
+	-- DEBUG TESTING SUPPORT
+	--Debug.Break()
+	--valueOfLocalMagneticAnomaly[1] = {magneticAnomalyValue = 175, referenceDirection = 5} --RefDir 5 = East, 13 = West 
+	--valueOfLocalMagneticAnomaly[2] = {magneticAnomalyValue = 145, referenceDirection = unknownValue} --RefDir 5 = East, 13 = West 
+
+	local magCount = #valueOfLocalMagneticAnomaly
+	
+	featurePortrayal:AddInstructions('LocalOffset:3.51,3.51;FontSize:10')
+
+	if magCount == 1 then
+		if valueOfLocalMagneticAnomaly[1].magneticAnomalyValue ~= nil then
+			if valueOfLocalMagneticAnomaly[1].magneticAnomalyValue ~= unknownValue then
+				if valueOfLocalMagneticAnomaly[1].referenceDirection ~= nil then
+					if valueOfLocalMagneticAnomaly[1].referenceDirection == 5 then
+						featurePortrayal:AddTextInstruction(EncodeString(valueOfLocalMagneticAnomaly[1].magneticAnomalyValue, '(%.0f°E)'), 30, 24, viewingGroup, 12)
+					elseif  valueOfLocalMagneticAnomaly[1].referenceDirection == 13 then -- West
+						featurePortrayal:AddTextInstruction(EncodeString(valueOfLocalMagneticAnomaly[1].magneticAnomalyValue, '(%.0f°W)'), 30, 24, viewingGroup, 12)
+					elseif valueOfLocalMagneticAnomaly[1].referenceDirection == unknownValue then
+						featurePortrayal:AddTextInstruction(EncodeString(valueOfLocalMagneticAnomaly[1].magneticAnomalyValue, '(±%.0f°)'), 30, 24, viewingGroup, 12)
+					end
+				else
+					featurePortrayal:AddTextInstruction(EncodeString(valueOfLocalMagneticAnomaly[1].magneticAnomalyValue, '(±%.0f°)'), 30, 24, viewingGroup, 12)
+				end
+			else
+				-- depict text "Local Magnetic Anomaly" - Colour (CHMGF to match LOCMAG51)
+				featurePortrayal:AddInstructions('TextAlignVertical:Center;FontWeight:Light;FontColor:CHMGF')
+				featurePortrayal:AddTextInstruction("Local Magnetic Anomaly", 30, 24, viewingGroup, 12)
+			end
+		else
+			error('Invalid primitive type or mariner settings passed to portrayal')
+		end
+	elseif magCount == 2 then
+		if valueOfLocalMagneticAnomaly[1].magneticAnomalyValue ~= nil and valueOfLocalMagneticAnomaly[2].magneticAnomalyValue ~= nil then
+
+			local txtStr
+			local dir1 = ''
+			local dir2 = ''
+			if valueOfLocalMagneticAnomaly[1].magneticAnomalyValue ~= unknownValue and valueOfLocalMagneticAnomaly[2].magneticAnomalyValue ~= unknownValue then
+				if (valueOfLocalMagneticAnomaly[1].referenceDirection ~= unknownValue and valueOfLocalMagneticAnomaly[2].referenceDirection ~= unknownValue) and
+					(valueOfLocalMagneticAnomaly[1].referenceDirection ~= nil and valueOfLocalMagneticAnomaly[2].referenceDirection ~= nil) then
+					if valueOfLocalMagneticAnomaly[1].referenceDirection == 5 then dir1 = 'E' end
+					if valueOfLocalMagneticAnomaly[2].referenceDirection == 5 then dir2 = 'E' end
+					if valueOfLocalMagneticAnomaly[1].referenceDirection == 13 then dir1 = 'W' end
+					if valueOfLocalMagneticAnomaly[2].referenceDirection == 13 then dir2 = 'W' end
+
+						txtStr = string.format('(%.0f°%s/%.0f°%s)',valueOfLocalMagneticAnomaly[1].magneticAnomalyValue, dir1, valueOfLocalMagneticAnomaly[2].magneticAnomalyValue, dir2)
+						featurePortrayal:AddTextInstruction(txtStr, 30, 24, viewingGroup, 12)
+				else
+					txtStr = string.format('(%.0f°%s/%.0f°%s)',valueOfLocalMagneticAnomaly[1].magneticAnomalyValue, dir1, valueOfLocalMagneticAnomaly[2].magneticAnomalyValue, dir2)
+					featurePortrayal:AddTextInstruction(txtStr, 30, 24, viewingGroup, 12)
+				end
+			elseif valueOfLocalMagneticAnomaly[1].magneticAnomalyValue == unknownValue or valueOfLocalMagneticAnomaly[2].magneticAnomalyValue == unknownValue then
+				featurePortrayal:AddInstructions('TextAlignVertical:Center;FontWeight:Light;FontColor:CHMGF')
+				featurePortrayal:AddTextInstruction("Local Magnetic Anomaly", 30, 24, viewingGroup, 12)
+			end
+		else
+			error('Invalid primitive type or mariner settings passed to portrayal')
+		end
+	end
+		
 	return viewingGroup
 end
