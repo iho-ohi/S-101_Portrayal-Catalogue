bebacdcc7c6fff97516f555d588ddeb59ea9a6af
David Grant <62906211+DavidGrant-NIWC@users.noreply.github.com>
2023-03-08 17:00:49 -0500

    Fixes #176

--- PortrayalCatalog/Rules/Runway.lua
+++ PortrayalCatalog/Rules/Runway.lua
@@ -9,9 +9,14 @@ function Runway(feature, featurePortrayal, contextParameters)
 		-- Simplified and paper chart points use the same symbolization
 		viewingGroup = 32240
 		if contextParameters.RadarOverlay then
-			featurePortrayal:AddInstructions('ViewingGroup:32240;DrawingPriority:18;DisplayPlane:OverRADAR;NullInstruction')
+			featurePortrayal:AddInstructions('ViewingGroup:32240;DrawingPriority:18;DisplayPlane:OverRADAR')
 		else
-			featurePortrayal:AddInstructions('ViewingGroup:32240;DrawingPriority:18;DisplayPlane:UnderRADAR;NullInstruction')
+			featurePortrayal:AddInstructions('ViewingGroup:32240;DrawingPriority:18;DisplayPlane:UnderRADAR')
+		end
+		if feature.categoryOfRunway and feature.categoryOfRunway == 1 then
+			featurePortrayal:AddInstructions('PointInstruction:AIRARE02')
+		else
+			featurePortrayal:AddInstructions('NullInstruction')
 		end
 	elseif feature.PrimitiveType == PrimitiveType.Curve then
 		viewingGroup = 32240
