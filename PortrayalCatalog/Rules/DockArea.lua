de3100febace6b83afd0cf0cea548832efa5047c
David Grant <62906211+DavidGrant-NIWC@users.noreply.github.com>
2023-10-16 14:54:31 -0400

    Fixes #269

--- PortrayalCatalog/Rules/DockArea.lua
+++ PortrayalCatalog/Rules/DockArea.lua
@@ -8,16 +8,17 @@ function DockArea(feature, featurePortrayal, contextParameters)
 	if feature.PrimitiveType == PrimitiveType.Surface then
 		viewingGroup = 22010
 		featurePortrayal:AddInstructions('ViewingGroup:22010;DrawingPriority:9;DisplayPlane:UnderRADAR;ColorFill:DEPVS')
-		if feature.featureName[1] and feature.featureName[1].name then
-			featurePortrayal:AddInstructions('LocalOffset:0,0;TextAlignHorizontal:Center;TextAlignVertical:Center;FontSize:10')
-			featurePortrayal:AddTextInstruction(EncodeString(GetFeatureName(feature, contextParameters)), 26, 24, 22010, 6)
-		end
+		-- #269: Dock area outline has incorrect priority and viewing group
 		if feature.condition then
 			featurePortrayal:SimpleLineStyle('dash',0.32,'CHBLK')
 		else
 			featurePortrayal:SimpleLineStyle('solid',0.32,'CHBLK')
 		end
 		featurePortrayal:AddInstructions('LineInstruction:_simple_')
+		if feature.featureName[1] and feature.featureName[1].name then
+			featurePortrayal:AddInstructions('LocalOffset:0,0;TextAlignHorizontal:Center;TextAlignVertical:Center;FontSize:10')
+			featurePortrayal:AddTextInstruction(EncodeString(GetFeatureName(feature, contextParameters)), 26, 24, 22010, 9)
+		end
 	else
 		error('Invalid primitive type or mariner settings passed to portrayal')
 	end
