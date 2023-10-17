f6c2b3d55f44f5c6bbea97ae56d4598eed15fa4e
David Grant <62906211+DavidGrant-NIWC@users.noreply.github.com>
2023-10-17 15:43:54 -0400

    Fixes #158

--- PortrayalCatalog/Rules/Default.lua
+++ PortrayalCatalog/Rules/Default.lua
@@ -11,7 +11,7 @@ function Default(feature, featurePortrayal, contextParameters)
 		else
 			featurePortrayal:AddInstructions('DisplayPlane:UnderRADAR')
 		end
-		featurePortrayal:AddInstructions('ViewingGroup:21010,unknown;DrawingPriority:15')
+		featurePortrayal:AddInstructions('ViewingGroup:21010;DrawingPriority:15')
 		featurePortrayal:AddInstructions('PointInstruction:QUESMRK1')
 	elseif (feature.PrimitiveType == PrimitiveType.Curve) then
 		if contextParameters.RadarOverlay then
@@ -19,17 +19,17 @@ function Default(feature, featurePortrayal, contextParameters)
 		else
 			featurePortrayal:AddInstructions('DisplayPlane:UnderRADAR')
 		end
-		featurePortrayal:AddInstructions('ViewingGroup:21010,unknown;DrawingPriority:15')
+		featurePortrayal:AddInstructions('ViewingGroup:21010;DrawingPriority:15')
 		featurePortrayal:AddInstructions('LineInstruction:QUESMRK1')
 	elseif (feature.PrimitiveType == PrimitiveType.Surface and contextParameters.PlainBoundaries) then
-		featurePortrayal:AddInstructions('ViewingGroup:21010,unknown;DrawingPriority:15;DisplayPlane:UnderRADAR')
+		featurePortrayal:AddInstructions('ViewingGroup:21010;DrawingPriority:15;DisplayPlane:UnderRADAR')
 		featurePortrayal:AddInstructions('PointInstruction:QUESMRK1')
 		featurePortrayal:SimpleLineStyle('dash',0.32,'CHMGD')
 		featurePortrayal:AddInstructions('LineInstruction:_simple_')
 	elseif (feature.PrimitiveType == PrimitiveType.Surface) then
-		featurePortrayal:AddInstructions('ViewingGroup:21010,unknown;DrawingPriority:15;DisplayPlane:UnderRADAR')
+		featurePortrayal:AddInstructions('ViewingGroup:21010;DrawingPriority:15;DisplayPlane:UnderRADAR')
 		featurePortrayal:AddInstructions('PointInstruction:QUESMRK1')
 		featurePortrayal:AddInstructions('LineInstruction:QUESMRK1')
 	end
-	return "21010,unknown"
+	return "21010"
 end
