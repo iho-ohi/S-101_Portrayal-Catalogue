37eedd4a66d7e0a640ad18e8e2ed50fe6f8d86c9
Mikan S <mikan.stamenkovich@navy.mil>
2023-10-20 15:08:40 -0400

    BuoyEmergency updated Paper symbol and Simplified

--- PortrayalCatalog/Rules/BuoyEmergencyWreckMarking.lua
+++ PortrayalCatalog/Rules/BuoyEmergencyWreckMarking.lua
@@ -5,9 +5,23 @@
 function BuoyEmergencyWreckMarking(feature, featurePortrayal, contextParameters)
   local viewingGroup
 
-	featurePortrayal:AddInstructions('AlertReference:NavHazard;Hover:true')
+  featurePortrayal:AddInstructions('AlertReference:NavHazard;Hover:true')
   
-  if feature.PrimitiveType == PrimitiveType.Point then
+
+	if feature.PrimitiveType == PrimitiveType.Point and contextParameters.SimplifiedSymbols then
+		viewingGroup = 27010
+		if contextParameters.RadarOverlay then
+			featurePortrayal:AddInstructions('ViewingGroup:27010;DrawingPriority:24;DisplayPlane:OverRADAR')
+		else
+			featurePortrayal:AddInstructions('ViewingGroup:27010;DrawingPriority:24;DisplayPlane:UnderRADAR')
+		end
+		featurePortrayal:AddInstructions('PointInstruction:BOYNDM01')
+
+ 		if feature.featureName[1] and feature.featureName[1].name then
+			featurePortrayal:AddInstructions('LocalOffset:-3.51,3.51;TextAlignHorizontal:End;FontSize:10')
+			featurePortrayal:AddTextInstruction(EncodeString(GetFeatureName(feature, contextParameters), 'by %s'), 21, 24, 27010, 24)
+		end 
+	elseif feature.PrimitiveType == PrimitiveType.Point then
 		viewingGroup = 27010
 		if contextParameters.RadarOverlay then
 			featurePortrayal:AddInstructions('ViewingGroup:27010;DrawingPriority:24;DisplayPlane:OverRADAR')
