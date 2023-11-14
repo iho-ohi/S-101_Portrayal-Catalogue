6fe98086e1f0ca22a8b1b917ea2c00f3c5711c4b
Mikan S <mikan.stamenkovich@navy.mil>
2023-11-14 13:56:44 -0500

    #276, New symbol for emergency wreck marking buoy (EWMB) [PSWG #117]

--- PortrayalCatalog/Rules/BuoyEmergencyWreckMarking.lua
+++ PortrayalCatalog/Rules/BuoyEmergencyWreckMarking.lua
@@ -1,6 +1,7 @@
 -- BuoyEmergencyWreckMarking main entry point.
 -- Feature Catalogue Version: 1.0.2 (2022/02/28)
 -- #162
+-- #276, [PSWG #117]
 
 function BuoyEmergencyWreckMarking(feature, featurePortrayal, contextParameters)
   local viewingGroup
@@ -14,7 +15,7 @@ function BuoyEmergencyWreckMarking(feature, featurePortrayal, contextParameters)
 		else
 			featurePortrayal:AddInstructions('ViewingGroup:27010;DrawingPriority:24;DisplayPlane:UnderRADAR')
 		end
-		featurePortrayal:AddInstructions('PointInstruction:BOYNDM01')
+		featurePortrayal:AddInstructions('PointInstruction:BOYNDM02')
 		if feature.featureName[1] and feature.featureName[1].name then
 			featurePortrayal:AddInstructions('LocalOffset:-3.51,3.51;TextAlignHorizontal:End;FontSize:10')
 			featurePortrayal:AddTextInstruction(EncodeString(GetFeatureName(feature, contextParameters), 'by %s'), 21, 24, 27010, 24)
