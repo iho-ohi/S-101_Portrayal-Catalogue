5b6a03c59113281b3daff86876a1cd49c15b6a8a
DavidGrant-NIWC <62906211+DavidGrant-NIWC@users.noreply.github.com>
2024-01-27 22:21:59 -0500

    Fixes #297

--- PortrayalCatalog/Rules/Runway.lua
+++ PortrayalCatalog/Rules/Runway.lua
@@ -6,24 +6,8 @@
 -- Runway main entry point.
 function Runway(feature, featurePortrayal, contextParameters)
 	local viewingGroup
-	local categoryOfRunway = feature.categoryOfRunway
 	
-	if feature.PrimitiveType == PrimitiveType.Point then
-		-- Simplified and paper chart points use the same symbolization
-		viewingGroup = 32240
-		if contextParameters.RadarOverlay then
-			featurePortrayal:AddInstructions('ViewingGroup:32240;DrawingPriority:12;DisplayPlane:OverRADAR')
-		else
-			featurePortrayal:AddInstructions('ViewingGroup:32240;DrawingPriority:12;DisplayPlane:UnderRADAR')
-		end
-		if categoryOfRunway == 1 then
-			featurePortrayal:AddInstructions('PointInstruction:AIRARE02')
-		elseif categoryOfRunway == 2 then
-			featurePortrayal:AddInstructions('PointInstruction:HELIPD02')
-		else
-			featurePortrayal:AddInstructions('NullInstruction')
-		end
-	elseif feature.PrimitiveType == PrimitiveType.Curve then
+	if feature.PrimitiveType == PrimitiveType.Curve then
 		viewingGroup = 32240
 		if contextParameters.RadarOverlay then
 			featurePortrayal:AddInstructions('ViewingGroup:32240;DrawingPriority:18;DisplayPlane:OverRADAR')
