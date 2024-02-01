84a354263b05a3ed762ccabffdfe14c7ae8e1bb2
DavidGrant-NIWC <62906211+DavidGrant-NIWC@users.noreply.github.com>
2024-02-01 12:16:27 -0500

    Fixes #279

--- PortrayalCatalog/Rules/DockArea.lua
+++ PortrayalCatalog/Rules/DockArea.lua
@@ -7,8 +7,7 @@ function DockArea(feature, featurePortrayal, contextParameters)
 
 	if feature.PrimitiveType == PrimitiveType.Surface then
 		viewingGroup = 22010
-		-- #279: Amend DockArea to remove color fill
-		featurePortrayal:AddInstructions('ViewingGroup:22010;DrawingPriority:9;DisplayPlane:UnderRADAR')
+		featurePortrayal:AddInstructions('ViewingGroup:22010;DrawingPriority:9;DisplayPlane:UnderRADAR;ColorFill:DEPVS')
 		-- #269: Dock area outline has incorrect priority and viewing group
 		if feature.condition then
 			featurePortrayal:SimpleLineStyle('dash',0.32,'CHBLK')
