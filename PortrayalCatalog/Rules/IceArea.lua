860e2347eda38eaaa5031621e4aaf73bf9fe21d0
DavidGrant-NIWC <62906211+DavidGrant-NIWC@users.noreply.github.com>
2024-04-29 11:46:32 -0400

    Fixes #358

--- PortrayalCatalog/Rules/IceArea.lua
+++ PortrayalCatalog/Rules/IceArea.lua
@@ -9,7 +9,7 @@ function IceArea(feature, featurePortrayal, contextParameters)
 		-- Plain and symbolized boundaries use the same symbolization
 		viewingGroup = 12410
 		featurePortrayal:AddInstructions('ViewingGroup:12410;DrawingPriority:9;DisplayPlane:UnderRADAR')
-		featurePortrayal:AddInstructions('ColorFill:NODTA')
+		featurePortrayal:AddInstructions('ColorFill:NODTA,0.5')
 		featurePortrayal:AddInstructions('AreaFillReference:ICEARE04')
 		featurePortrayal:SimpleLineStyle('dash',0.32,'CHGRD')
 		featurePortrayal:AddInstructions('LineInstruction:_simple_')
