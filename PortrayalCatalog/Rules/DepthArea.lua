fe5426baa946b4245213890fe083200e752254ff
DavidGrant-NIWC <62906211+DavidGrant-NIWC@users.noreply.github.com>
2024-05-01 14:56:09 -0400

    Fixes #372

--- PortrayalCatalog/Rules/DepthArea.lua
+++ PortrayalCatalog/Rules/DepthArea.lua
@@ -10,7 +10,16 @@ function DepthArea(feature, featurePortrayal, contextParameters)
 		-- Plain and symbolized boundaries use the same symbolization
 		viewingGroup = 13030
 		featurePortrayal:AddInstructions('ViewingGroup:13030;DrawingPriority:3;DisplayPlane:UnderRADAR')
-		DEPARE03(feature, featurePortrayal, contextParameters, viewingGroup)
+		
+		if feature.depthRangeMinimumValue or feature.depthRangeMaximumValue then
+			DEPARE03(feature, featurePortrayal, contextParameters, viewingGroup)
+		else
+			-- Incompletely surveyed area
+			featurePortrayal:AddInstructions('ColorFill:NODTA')
+			featurePortrayal:AddInstructions('AreaFillReference:PRTSUR01')
+			featurePortrayal:SimpleLineStyle('solid',0.64,'CHGRD')
+			featurePortrayal:AddInstructions('LineInstruction:_simple_')
+		end
 	else
 		error('Invalid primitive type or mariner settings passed to portrayal')
 	end
