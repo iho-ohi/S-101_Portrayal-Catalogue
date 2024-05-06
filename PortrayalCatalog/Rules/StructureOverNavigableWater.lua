d3615fd9f9101bbe206af9ef8da9bfc6f0daf234
DavidGrant-NIWC <62906211+DavidGrant-NIWC@users.noreply.github.com>
2024-05-06 10:25:29 -0400

    #360: add vertical clearance label to StructureOverNavigableWater

--- PortrayalCatalog/Rules/StructureOverNavigableWater.lua
+++ PortrayalCatalog/Rules/StructureOverNavigableWater.lua
@@ -11,10 +11,10 @@ function StructureOverNavigableWater(feature, featurePortrayal, contextParameter
 		featurePortrayal:SimpleLineStyle('solid',0.32,'CHBLK')
 		featurePortrayal:AddInstructions('LineInstruction:_simple_')
 
-		--if feature.verticalClearanceFixed and feature.verticalClearanceFixed.verticalClearanceValue then
-			--featurePortrayal:AddInstructions('LocalOffset:3.51,-3.51;FontColor:CHBLK')
-			--featurePortrayal:AddTextInstruction(EncodeString(feature.verticalClearanceFixed.verticalClearanceValue, 'clr %4.1f'), 11, 24, 12210, 24)
-		--end
+		if feature.verticalClearanceFixed and feature.verticalClearanceFixed.verticalClearanceValue then
+			featurePortrayal:AddInstructions('LocalOffset:3.51,-3.51;FontColor:CHBLK')
+			featurePortrayal:AddTextInstruction(EncodeString(feature.verticalClearanceFixed.verticalClearanceValue, 'clr %4.1f'), 11, 24, 12210, 24)
+		end
 	else
 		error('Invalid primitive type or mariner settings passed to portrayal')
 	end
