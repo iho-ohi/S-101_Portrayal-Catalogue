1dd982fc871e62f6c9d2162fe79c7d76c1a48e95
DavidGrant-NIWC <62906211+DavidGrant-NIWC@users.noreply.github.com>
2024-05-08 13:43:43 -0400

    #386: updates to MooringArea

--- PortrayalCatalog/Rules/MooringArea.lua
+++ PortrayalCatalog/Rules/MooringArea.lua
@@ -8,100 +8,76 @@ require 'RESTRN01'
 
 function MooringArea(feature, featurePortrayal, contextParameters)
 
-	local viewingGroup = 12410  -- categoryOfMooringArea = 1, 2, or Unknown
-	local drawingPriority = 18
-	local roStr1 = 'ViewingGroup:' .. viewingGroup ..';DrawingPriority:' .. drawingPriority ..';DisplayPlane:OverRADAR'
-	local roStr2 = 'ViewingGroup:' .. viewingGroup ..';DrawingPriority:' .. drawingPriority ..';DisplayPlane:UnderRADAR'
-	local hasName = false
+	local viewingGroup = 26220
 
 	featurePortrayal:AddInstructions('AlertReference:ProhAre,53023,53023')
 
-	local CAMA = feature.categoryOfMooringArea;
-
-	-- adjust viewing group for mooring area for tankers, based on S-52
-	if CAMA == 3 then
-		viewingGroup = 32440
-		drawingPriority = 18
-		roStr1 = 'ViewingGroup:' .. viewingGroup ..';DrawingPriority:' .. drawingPriority ..';DisplayPlane:OverRADAR'
-		roStr2 = 'ViewingGroup:' .. viewingGroup ..';DrawingPriority:' .. drawingPriority ..';DisplayPlane:UnderRADAR'
-	end
-
-	--
 	-- #309, "Visitors" or "Tankers" string placement to add to text below symbol
-	--
-	local function AddVisitorTankerName()
-		local xyPos
-		local cstr = '' 
+	local function AddVisitorTankerName(feature, localOffset)
 
-		--
-		-- 2 : mooring area for visitors, 3 : mooring area for tankers
-		--
-		if CAMA == 2 then
-			cstr = "Visitors"
-		elseif CAMA == 3 then
-			cstr = "Tankers"
-		end
-		
-		if (CAMA == 2 or CAMA == 3) then
-			if (hasName) then
-				xyPos = '-3.51,-11.02'	-- place below name
-			else
-		 		xyPos = '-3.51,-5.02'	-- place tighter against symbol
+		-- 1 : Small craft mooring area, 2 : mooring area for visitors, 3 : mooring area for tankers
+		if contains(feature.categoryOfMooringArea, {1, 2, 3}) then
+			local labels = {}
+			
+			if contains(1, feature.categoryOfMooringArea) then
+				labels[#labels + 1] = "Small Crafts"
 			end
-			 featurePortrayal:AddInstructions('LocalOffset:' .. xyPos .. ';FontSize:10;FontSlant:Italics;FontColor:CHMGD')
-			 featurePortrayal:AddTextInstruction(cstr, 26, 24, viewingGroup, 18)
+			if contains(2, feature.categoryOfMooringArea) then
+				labels[#labels + 1] = "Visitors"
+			end
+			if contains(3, feature.categoryOfMooringArea) then
+				labels[#labels + 1] = "Tankers"
+			end
+			
+			featurePortrayal:AddInstructions('LocalOffset:' .. localOffset .. ';TextAlignHorizontal:Center;TextAlignVertical:Top;FontSlant:Italics;FontColor:CHGRF')
+			featurePortrayal:AddTextInstruction(safeConcat(labels, ","), 24, 24, viewingGroup, 18)
 		end
 	end	
 
-
 	if feature.PrimitiveType == PrimitiveType.Point then
 		-- Simplified and paper chart points use the same symbolization
-		
 		if contextParameters.RadarOverlay then
-			featurePortrayal:AddInstructions(roStr1)
+			featurePortrayal:AddInstructions('ViewingGroup:26220;DrawingPriority:18;DisplayPlane:OverRADAR')
 		else
-			featurePortrayal:AddInstructions(roStr2)
+			featurePortrayal:AddInstructions('ViewingGroup:26220;DrawingPriority:18;DisplayPlane:OverRADAR')
 		end
 
 		featurePortrayal:AddInstructions('PointInstruction:BOYMOR02')
 
 		if feature.featureName[1] and feature.featureName[1].name then
-			hasName = true
-			featurePortrayal:AddInstructions('LocalOffset:-3.51,-7.02;FontSize:10;FontColor:CHBLK')
+			featurePortrayal:AddInstructions('LocalOffset:3,2;FontColor:CHBLK')
 	    	featurePortrayal:AddTextInstruction(EncodeString(GetFeatureName(feature, contextParameters)), 26, 24, viewingGroup, 18)
 		end
 
-		AddVisitorTankerName()
+		AddVisitorTankerName(feature, '0,-2')
 
 	elseif feature.PrimitiveType == PrimitiveType.Surface and contextParameters.PlainBoundaries then
-		featurePortrayal:AddInstructions('ViewingGroup:' .. viewingGroup ..';DrawingPriority:9;DisplayPlane:UnderRADAR')
+		featurePortrayal:AddInstructions('ViewingGroup:26220;DrawingPriority:9;DisplayPlane:UnderRADAR')
 		featurePortrayal:AddInstructions('PointInstruction:BOYMOR51')
 		featurePortrayal:SimpleLineStyle('dash',0.64,'CHGRF')
 		featurePortrayal:AddInstructions('LineInstruction:_simple_')
 
+		RESTRN01(feature, featurePortrayal, contextParameters, viewingGroup)
+		
 		if feature.featureName[1] and feature.featureName[1].name then
-			hasName = true
-			featurePortrayal:AddInstructions('LocalOffset:-3.51,-9.02;FontSize:10;FontColor:CHBLK')
+			featurePortrayal:AddInstructions('LocalOffset:5,2;FontColor:CHBLK')
 	    	featurePortrayal:AddTextInstruction(EncodeString(GetFeatureName(feature, contextParameters)), 26, 24, viewingGroup, 9)
 		end
 
-		AddVisitorTankerName()
-
-		RESTRN01(feature, featurePortrayal, contextParameters, viewingGroup)
+		AddVisitorTankerName(feature, '0,-6')
 	elseif feature.PrimitiveType == PrimitiveType.Surface then
-		featurePortrayal:AddInstructions('ViewingGroup:' .. viewingGroup ..';DrawingPriority:9;DisplayPlane:UnderRADAR')
+		featurePortrayal:AddInstructions('ViewingGroup:26220;DrawingPriority:9;DisplayPlane:UnderRADAR')
 		featurePortrayal:AddInstructions('PointInstruction:BOYMOR51')
 		featurePortrayal:AddInstructions('LineInstruction:NAVARE52')
 
+		RESTRN01(feature, featurePortrayal, contextParameters, viewingGroup)
+
 		if feature.featureName[1] and feature.featureName[1].name then
-			hasName = true
-			featurePortrayal:AddInstructions('LocalOffset:-3.51,-9.02;FontSize:10;FontColor:CHBLK')
+			featurePortrayal:AddInstructions('LocalOffset:5,2;FontColor:CHBLK')
 	    	featurePortrayal:AddTextInstruction(EncodeString(GetFeatureName(feature, contextParameters)), 26, 24, viewingGroup, 9)
 		end
 
-		AddVisitorTankerName()
-
-		RESTRN01(feature, featurePortrayal, contextParameters, viewingGroup)
+		AddVisitorTankerName(feature, '0,-6')
 	else
 		error('Invalid primitive type or mariner settings passed to portrayal')
 	end
