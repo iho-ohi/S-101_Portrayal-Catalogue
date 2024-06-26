6e2e00ff066d43b845d2d718e9781a22aee782c1
DavidGrant-NIWC <62906211+DavidGrant-NIWC@users.noreply.github.com>
2024-06-26 12:34:30 -0400

    Fixes #399

--- PortrayalCatalog/Rules/TextPlacement.lua
+++ PortrayalCatalog/Rules/TextPlacement.lua
@@ -2,5 +2,10 @@
 
 -- Main entry point for feature type.
 function TextPlacement(feature, featurePortrayal, contextParameters)
+	-- NOTE:
+	--
+	-- TextPlacement is not implemented via this file; this routine will never be called (because it is short-circuited in main.lua).
+	-- TextPlacement is processed when the feature(s) to which it is associated are processed.
+	-- The implementation is in PortrayalModel.lua, mostly as part of featurePortrayal:AddTextInstruction() and featurePortrayal:GetFeatureName().
 	return -1
 end
