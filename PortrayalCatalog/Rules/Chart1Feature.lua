94c6d248f1d933b335013637efa19a64fcd09e8d
DavidGrant-NIWC <62906211+DavidGrant-NIWC@users.noreply.github.com>
2024-07-16 16:06:14 -0400

    Fixes #389

---
+++ PortrayalCatalog/Rules/Chart1Feature.lua
@@ -0,0 +1,29 @@
+-- Chart1Feature main entry point.
+function Chart1Feature(feature, featurePortrayal, contextParameters)
+
+	if feature.PrimitiveType ~= PrimitiveType.Point and feature.PrimitiveType ~= PrimitiveType.Curve and feature.PrimitiveType ~= PrimitiveType.Surface then
+		error('Invalid primitive type or mariner settings passed to portrayal')
+	end
+
+	local viewingGroup = 21020
+	
+	featurePortrayal:AddInstructions('DisplayPlane:UnderRADAR')
+
+	local featureName = EncodeString(GetFeatureName(feature, contextParameters))
+	if featureName then
+		-- Add default text parameters
+		featurePortrayal:AddInstructions('TextAlignHorizontal:Center;TextAlignVertical:Center;FontColor:CHBLK')
+	end	
+
+	if feature.drawingInstruction then			
+		-- may modify the default text parameters set above
+		featurePortrayal:AddInstructions(feature.drawingInstruction)
+	end
+
+	if featureName then
+		-- args: text, textViewingGroup, textPriority, featureViewingGroup
+        featurePortrayal:AddTextInstruction(featureName, 21, 24, viewingGroup)
+    end
+
+	return viewingGroup
+end
