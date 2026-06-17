5f959022b6c5f597a0245087b97809eee3b7941a
abadhs_spawar <185529352+HyacinthAbad-NIWC@users.noreply.github.com>
2026-06-17 11:19:35 -0400

    Fixed missing vertical clearance for bridges Span features

--- PortrayalCatalog/Rules/SpanFixed.lua
+++ PortrayalCatalog/Rules/SpanFixed.lua
@@ -1,43 +1,45 @@
 require 'S101AttributeSupport'
 
 -- from S-57 BRIDGE, CATBRG=1 (fixed bridge)
+
 function SpanFixed(feature, featurePortrayal, contextParameters)
-	if feature.verticalClearanceFixed.verticalClearanceValue <= contextParameters.SafetyHeight then
-		featurePortrayal:AddInstructions('AlertReference:NavHazard')
-	end
 
-	if feature.PrimitiveType == PrimitiveType.Curve then
-		if contextParameters.RadarOverlay then
-			featurePortrayal:AddInstructions('ViewingGroup:12210;DrawingPriority:24;DisplayPlane:OverRadar')
-		else
-			featurePortrayal:AddInstructions('ViewingGroup:12210;DrawingPriority:24;DisplayPlane:UnderRadar')
-		end
-		featurePortrayal:SimpleLineStyle('solid',1.6,'CHGRD')
-		featurePortrayal:AddInstructions('LineInstruction:_simple_')
+    -- If clearance is missing (nil) OR it is <= SafetyHeight, trigger the hazard alert.
+    if feature.verticalClearanceFixed.verticalClearanceValue == nil or feature.verticalClearanceFixed.verticalClearanceValue <= contextParameters.SafetyHeight then
+            featurePortrayal:AddInstructions('AlertReference:NavHazard')
+    end
 
-	elseif feature.PrimitiveType == PrimitiveType.Surface then
-		if contextParameters.RadarOverlay then
-			featurePortrayal:AddInstructions('ViewingGroup:12210;DrawingPriority:24;DisplayPlane:OverRadar')
-		else
-			featurePortrayal:AddInstructions('ViewingGroup:12210;DrawingPriority:24;DisplayPlane:UnderRadar')
-		end
-		featurePortrayal:SimpleLineStyle('solid',1.28,'CHGRD')
-		featurePortrayal:AddInstructions('LineInstruction:_simple_')
 
-	else
-		error('Invalid primitive type or mariner settings passed to portrayal')
-	end
+    if feature.PrimitiveType == PrimitiveType.Curve then
+        if contextParameters.RadarOverlay then
+            featurePortrayal:AddInstructions('ViewingGroup:12210;DrawingPriority:24;DisplayPlane:OverRadar')
+        else
+            featurePortrayal:AddInstructions('ViewingGroup:12210;DrawingPriority:24;DisplayPlane:UnderRadar')
+        end
+        featurePortrayal:SimpleLineStyle('solid',1.6,'CHGRD')
+        featurePortrayal:AddInstructions('LineInstruction:_simple_')
+    elseif feature.PrimitiveType == PrimitiveType.Surface then
+        if contextParameters.RadarOverlay then
+            featurePortrayal:AddInstructions('ViewingGroup:12210;DrawingPriority:24;DisplayPlane:OverRadar')
+        else
+            featurePortrayal:AddInstructions('ViewingGroup:12210;DrawingPriority:24;DisplayPlane:UnderRadar')
+        end
+        featurePortrayal:SimpleLineStyle('solid',1.28,'CHGRD')
+        featurePortrayal:AddInstructions('LineInstruction:_simple_')
+    else
+        error('Invalid primitive type or mariner settings passed to portrayal')
+    end
 
-	-- Note: SpanFixed binds horizontalClearanceFixed and verticalClearanceFixed. It doesn't bind featureName.
-	if HasClearance(feature) then
-		local yOffset = 0
-		featurePortrayal:AddInstructions('TextAlignHorizontal:Center')
-		if feature.PrimitiveType == PrimitiveType.Curve then
-			featurePortrayal:AddInstructions('TextAlignVertical:Center')
-			yOffset = -3.51
-		end
-		PortrayClearances(feature, featurePortrayal, contextParameters, 12210, 0, yOffset)
-	end
+    -- Note: SpanFixed binds horizontalClearanceFixed and verticalClearanceFixed. It doesn't bind featureName.
+    if HasClearance(feature) then
+        local yOffset = 0
+        featurePortrayal:AddInstructions('TextAlignHorizontal:Center')
+        if feature.PrimitiveType == PrimitiveType.Curve then
+            featurePortrayal:AddInstructions('TextAlignVertical:Center')
+            yOffset = -3.51
+        end
+        PortrayClearances(feature, featurePortrayal, contextParameters, 12210, 0, yOffset)
+    end
 
-	return 12210
+    return 12210
 end
--- PortrayalCatalog/Rules/SpanOpening.lua
+++ PortrayalCatalog/Rules/SpanOpening.lua
@@ -1,43 +1,48 @@
 require 'S101AttributeSupport'
 
 -- from S-57 BRIDGE, CATBRG=2 (opening bridge)
+
 function SpanOpening(feature, featurePortrayal, contextParameters)
-	if feature.verticalClearanceClosed.verticalClearanceValue <= contextParameters.SafetyHeight then
-		featurePortrayal:AddInstructions('AlertReference:NavHazard')
-	end
 
-	if feature.PrimitiveType == PrimitiveType.Curve then
-		if contextParameters.RadarOverlay then
-			featurePortrayal:AddInstructions('ViewingGroup:12210;DrawingPriority:24;DisplayPlane:OverRadar')
-		else
-			featurePortrayal:AddInstructions('ViewingGroup:12210;DrawingPriority:24;DisplayPlane:UnderRadar')
-		end
-		featurePortrayal:SimpleLineStyle('solid',1.6,'CHGRD')
-		featurePortrayal:AddInstructions('LineInstruction:_simple_')
-		featurePortrayal:AddInstructions('PointInstruction:BRIDGE01')
+    local clearance = feature.verticalClearanceClosed and feature.verticalClearanceClosed.verticalClearanceValue
 
-	elseif feature.PrimitiveType == PrimitiveType.Surface then
-		if contextParameters.RadarOverlay then
-			featurePortrayal:AddInstructions('ViewingGroup:12210;DrawingPriority:24;DisplayPlane:OverRadar')
-		else
-			featurePortrayal:AddInstructions('ViewingGroup:12210;DrawingPriority:24;DisplayPlane:UnderRadar')
-		end
-		featurePortrayal:AddInstructions('PointInstruction:BRIDGE01')
-		featurePortrayal:SimpleLineStyle('solid',1.28,'CHGRD')
-		featurePortrayal:AddInstructions('LineInstruction:_simple_')
-	else
-		error('Invalid primitive type or mariner settings passed to portrayal')
-	end
+    -- If clearance is missing (nil) OR it is <= SafetyHeight, trigger the hazard alert.
+    if feature.verticalClearanceClosed.verticalClearanceValue == nil or feature.verticalClearanceClosed.verticalClearanceValue <= contextParameters.SafetyHeight then
+            featurePortrayal:AddInstructions('AlertReference:NavHazard')
+    end
 
-	-- Binds horizontalClearanceFixed, verticalClearanceClosed, verticalClearanceOpen
-	if HasClearance(feature) then
-		if feature.PrimitiveType == PrimitiveType.Curve then
-			featurePortrayal:AddInstructions('TextAlignHorizontal:Center;TextAlignVertical:Center')
-			PortrayClearances(feature, featurePortrayal, contextParameters, 12210, 0, -3.51)
-		else
-			PortrayClearances(feature, featurePortrayal, contextParameters, 12210, 3.51, 0)
-		end
-	end
 
-	return 12210
+    if feature.PrimitiveType == PrimitiveType.Curve then
+        if contextParameters.RadarOverlay then
+            featurePortrayal:AddInstructions('ViewingGroup:12210;DrawingPriority:24;DisplayPlane:OverRadar')
+        else
+            featurePortrayal:AddInstructions('ViewingGroup:12210;DrawingPriority:24;DisplayPlane:UnderRadar')
+        end
+        featurePortrayal:SimpleLineStyle('solid',1.6,'CHGRD')
+        featurePortrayal:AddInstructions('LineInstruction:_simple_')
+        featurePortrayal:AddInstructions('PointInstruction:BRIDGE01')
+    elseif feature.PrimitiveType == PrimitiveType.Surface then
+        if contextParameters.RadarOverlay then
+            featurePortrayal:AddInstructions('ViewingGroup:12210;DrawingPriority:24;DisplayPlane:OverRadar')
+        else
+            featurePortrayal:AddInstructions('ViewingGroup:12210;DrawingPriority:24;DisplayPlane:UnderRadar')
+        end
+        featurePortrayal:AddInstructions('PointInstruction:BRIDGE01')
+        featurePortrayal:SimpleLineStyle('solid',1.28,'CHGRD')
+        featurePortrayal:AddInstructions('LineInstruction:_simple_')
+    else
+        error('Invalid primitive type or mariner settings passed to portrayal')
+    end
+
+    -- Binds horizontalClearanceFixed, verticalClearanceClosed, verticalClearanceOpen
+    if HasClearance(feature) then
+        if feature.PrimitiveType == PrimitiveType.Curve then
+            featurePortrayal:AddInstructions('TextAlignHorizontal:Center;TextAlignVertical:Center')
+            PortrayClearances(feature, featurePortrayal, contextParameters, 12210, 0, -3.51)
+        else
+            PortrayClearances(feature, featurePortrayal, contextParameters, 12210, 3.51, 0)
+        end
+    end
+
+    return 12210
 end
