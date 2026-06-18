0554d2db3e4ce693c58b0a38e430386050226753
abadhs_spawar <185529352+HyacinthAbad-NIWC@users.noreply.github.com>
2026-06-18 11:49:15 -0400

    cable area subwg issue, waiting for new symbol

--- PortrayalCatalog/Rules/CableArea.lua
+++ PortrayalCatalog/Rules/CableArea.lua
@@ -1,27 +1,58 @@
-
 -- Referenced portrayal rules.
 require 'RESTRN01'
 
 -- Cable area main entry point.
 function CableArea(feature, featurePortrayal, contextParameters)
-	local viewingGroup
+    local viewingGroup = 34030
 
-	if feature.PrimitiveType == PrimitiveType.Surface and contextParameters.PlainBoundaries then
-		viewingGroup = 34030
-		featurePortrayal:AddInstructions('ViewingGroup:34030;DrawingPriority:9;DisplayPlane:UnderRadar')
-		featurePortrayal:AddInstructions('PointInstruction:CBLARE51')
-		featurePortrayal:SimpleLineStyle('dash',0.64,'CHMGD')
-		featurePortrayal:AddInstructions('LineInstruction:_simple_')
-		RESTRN01(feature, featurePortrayal, contextParameters, viewingGroup)
-	elseif feature.PrimitiveType == PrimitiveType.Surface then
-		viewingGroup = 34030
-		featurePortrayal:AddInstructions('ViewingGroup:34030;DrawingPriority:9;DisplayPlane:UnderRadar')
-		featurePortrayal:AddInstructions('PointInstruction:CBLARE51')
-		featurePortrayal:AddInstructions('LineInstruction:CBLARE51')
-		RESTRN01(feature, featurePortrayal, contextParameters, viewingGroup)
-	else
-		error('Invalid primitive type or mariner settings passed to portrayal')
-	end
+    if feature.PrimitiveType == PrimitiveType.Surface and contextParameters.PlainBoundaries then
+        
+        -- Radar Overlay Check
+        if contextParameters.RadarOverlay then
+            featurePortrayal:AddInstructions('ViewingGroup:34030;DrawingPriority:9;DisplayPlane:OverRadar')
+        else
+            featurePortrayal:AddInstructions('ViewingGroup:34030;DrawingPriority:9;DisplayPlane:UnderRadar')
+        end
 
-	return viewingGroup
+        -- Cables 4 & 5 (Telephone / Telegraph)
+        if feature.categoryOfCable == 4 or feature.categoryOfCable == 5 then 
+            featurePortrayal:AddInstructions('PointInstruction:CBLARE51') --need to change to CBLARE52 
+            featurePortrayal:SimpleLineStyle('dash', 0.64, 'CHMGD')
+            featurePortrayal:AddInstructions('LineInstruction:_simple_')
+            RESTRN01(feature, featurePortrayal, contextParameters, viewingGroup)
+            
+        -- Default (Other Cables)
+        else
+            featurePortrayal:AddInstructions('PointInstruction:CBLARE51')
+            featurePortrayal:SimpleLineStyle('dash', 0.64, 'CHMGD')
+            featurePortrayal:AddInstructions('LineInstruction:_simple_')
+            RESTRN01(feature, featurePortrayal, contextParameters, viewingGroup)
+        end
+
+    elseif feature.PrimitiveType == PrimitiveType.Surface then
+        
+        if contextParameters.RadarOverlay then
+            featurePortrayal:AddInstructions('ViewingGroup:34030;DrawingPriority:9;DisplayPlane:OverRadar')
+        else
+            featurePortrayal:AddInstructions('ViewingGroup:34030;DrawingPriority:9;DisplayPlane:UnderRadar')
+        end
+
+        -- Cables 4 & 5 (Telephone / Telegraph)
+        if feature.categoryOfCable == 4 or feature.categoryOfCable == 5 then
+            featurePortrayal:AddInstructions('PointInstruction:CBLARE51') --need to change to CBLARE52
+            featurePortrayal:AddInstructions('LineInstruction:CBLARE51') --need to change to CBLARE52
+            RESTRN01(feature, featurePortrayal, contextParameters, viewingGroup)
+            
+        -- Default (Other Cables)
+        else
+            featurePortrayal:AddInstructions('PointInstruction:CBLARE51')
+            featurePortrayal:AddInstructions('LineInstruction:CBLARE51')
+            RESTRN01(feature, featurePortrayal, contextParameters, viewingGroup)
+        end
+
+    else
+        error('Invalid primitive type or mariner settings passed to portrayal')
+    end
+
+    return viewingGroup
 end
