c3e70511649ced1c5755ec56d7628b4d62740702
abadhs_spawar <185529352+HyacinthAbad-NIWC@users.noreply.github.com>
2026-06-29 09:11:03 -0400

    cable updates

--- PortrayalCatalog/Rules/CableSubmarine.lua
+++ PortrayalCatalog/Rules/CableSubmarine.lua
@@ -2,6 +2,7 @@
 -- Cable Submarine main entry point.
 function CableSubmarine(feature, featurePortrayal, contextParameters)
 	local viewingGroup
+	local status = feature.status or feature.STATUS
 
 	if feature.PrimitiveType == PrimitiveType.Curve then
 
@@ -11,21 +12,25 @@ function CableSubmarine(feature, featurePortrayal, contextParameters)
 			featurePortrayal:AddInstructions('DisplayPlane:UnderRadar')
 		end
 
-		if feature.categoryOfCable == 6 then
+		if contains(4, feature.status) then
+			viewingGroup = 34070
+			featurePortrayal:AddInstructions('ViewingGroup:34070;DrawingPriority:9')
+			featurePortrayal:AddInstructions('LineInstruction:CBLSUB07') --changed to CBLSUB08
+		elseif feature.categoryOfCable == 6 then
 			viewingGroup = 24010
 			featurePortrayal:AddInstructions('ViewingGroup:24010;DrawingPriority:18')
-			featurePortrayal:SimpleLineStyle('dash',0.32,'CHMGD')
-			featurePortrayal:AddInstructions('LineInstruction:_simple_')
+			--featurePortrayal:SimpleLineStyle('dash',0.32,'CHMGD')
+			featurePortrayal:AddInstructions('LineInstruction:CBLSUB08') --changed to CBLSUB08
+			--featurePortrayal:AddInstructions('LineInstruction:_simple_')
 		elseif feature.categoryOfCable == 10 then  --value of 10 is general telecommunication lines (incorporates 4 and 5)
 			featurePortrayal:AddInstructions('ViewingGroup:34070;DrawingPriority:9')
 			featurePortrayal:AddInstructions('LineInstruction:CBLSUB07') -- changed to CBLSUB07
-		elseif feature.status == 4 then
-			featurePortrayal:AddInstructions('ViewingGroup:34070;DrawingPriority:9')
-			featurePortrayal:AddInstructions('LineInstruction:CBLSUB08') --changed to CBLSUB08
+	
 		else
 			viewingGroup = 34070
 			featurePortrayal:AddInstructions('ViewingGroup:34070;DrawingPriority:9')
 			featurePortrayal:AddInstructions('LineInstruction:CBLSUB06')
+
 		end
 	else
 		error('Invalid primitive type or mariner settings passed to portrayal')
